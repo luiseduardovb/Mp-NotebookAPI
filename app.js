0909bce02499a0c38577ed95c6dfdee562676285
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-05 09:31:18 +0300

    As a user, I can create new notebooks

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 //Data
 const notebooks = require("./notebooks");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/notebooks", (req, res) => {
   res.json(notebooks);
+});
+
+app.post("/notebooks", (req, res) => {
+  const id = notebooks[notebooks.length - 1].id + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newNotebook = { id, slug, ...req.body };
+  notebooks.push(newNotebook);
+  res.status(201).json(newNotebook);
 });
 
 app.listen(8000, () => {
