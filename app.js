0ed86af29facc23837caf398399e9d2ef53ff839
luiseduardovb <le_velasquez@yahoo.com>
2020-08-05 08:49:56 +0300

    create new notebook not finished

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
+//Data
+const notebooks = require("./notebooks");
 
 const app = express();
+app.use(cors());
+
+app.get("/notebooks", (req, res) => {
+  res.json(notebooks);
+});
 
 app.listen(8000, () => {
   console.log("The application is running on localhost:8000");
---
+++ notebooks.js
@@ -0,0 +1,8 @@
+const notebooks = [
+  { id: 1, name: "to-do" },
+  { id: 2, name: "javascript" },
+  { id: 3, name: "reactjs" },
+  { id: 4, name: "express" },
+];
+
+module.exports = notebooks;
