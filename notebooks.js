2373ba56bfe351bc4218867f0f61982c9d684bbd
luiseduardovb <le_velasquez@yahoo.com>
2020-08-05 10:19:08 +0300

    detail error

--- notebooks.js
+++ notebooks.js
@@ -1,8 +1,8 @@
 const notebooks = [
-  { id: 1, name: "to-do" },
-  { id: 2, name: "javascript" },
-  { id: 3, name: "reactjs" },
-  { id: 4, name: "express" },
+  { id: 1, name: "to-do", slug: "to-do" },
+  { id: 2, name: "javascript", slug: "javascript" },
+  { id: 3, name: "reactjs", slug: "reactjs" },
+  { id: 4, name: "express", slug: "express" },
 ];
 
 module.exports = notebooks;
